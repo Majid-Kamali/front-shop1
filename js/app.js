ced48a08764a74d24a4d74a24ae17b3cd2748947
Majid-Kamali <majidkamali04@gmail.com>
2024-08-23 20:09:09 +0330

    coding toolbar search result

--- js/app.js
+++ js/app.js
@@ -3,6 +3,8 @@ const typingDelay = 300;
 
 const searchInput = document.querySelector('#search-input');
 const searchBtn = document.querySelector('.search__bar__btn');
+const searchResults = document.querySelector('.search__results');
+
 searchInput.addEventListener('input', () => {
 
     const query = searchInput.value.trim();
@@ -29,58 +31,59 @@ function fetchResults(query) {
     // fetch(`/search?q=${query}`)
     fetch('https://fakestoreapi.com/products?limit=3')
         .then(response => response.json())
-        
+
         .then(data => displayResults(data))
         .catch(error => console.error('Error fetching data:', error));
 }
 
 function displayResults(results) {
+   
     const resultsHeader = document.querySelector('.search__results__header');
     const resultsList = document.querySelector('.search__results__list');
-    const resultsFooter = document.querySelector('.search__results__footer');
-    resultsHeader.innerHTML=''
-    resultsList.innerHTML=''
-    resultsFooter.innerHTML=''
-        
+    resultsHeader.innerHTML = ''
+    resultsList.innerHTML = ''
+    searchResults.classList.replace('hidden','block');
+    
+
     resultsHeader.innerHTML = `
        <a class="search__results__header__count" href="#">${results.length}تعداد نتیجه یافت شده</a>    
     `
     results.forEach(result => {
         resultsList.insertAdjacentHTML('beforeend', `
             
-             <div class="search__results__list__card">
+             <a href="#" class="search__results__list__card">
                 <div class="search__results__list__card__thumbnail">
                     <img class="search__results__list__card__thumbnail__img" src="${result.image}"
                         alt="${result.title}">
                 </div>
                 <div class="search__results__list__card__content">
-                    <h5 class="search__results__list__card__content__title">${result.title}</h5>
-                    <p class="search__results__list__card__content__description">
-                        ${result.description}</p>
-                   <p> 
-                   <span class="search__results__list__card__content__rating">🥇 ${result.rating.rate}</span>
-                   &nbsp;
-                   <span class="search__results__list__card__content__price">${result.price}  تومان </span>
+                    
+                        <h5 class="search__results__list__card__content__title">${result.title}</h5>
+                        <p class="search__results__list__card__content__description">
+                            ${result.description}</p>
+                    <p> 
+                        <span class="search__results__list__card__content__rating">🥇 ${result.rating.rate}</span>
+                        <span class="search__results__list__card__content__price">${result.price}  تومان </span>
                     </p>
+                  
                 </div>
-            </div>
+            </a>
             
         `);
     });
 
-    resultsFooter.innerHTML = `
-        <a class="search__results__footer__btn" href="#">مشاهده همه نتایج</a>
-   
-    `
+  
 }
 
 function clearResults() {
+    searchResults.classList.replace('block','hidden');
+
     const resultsHeader = document.querySelector('.search__results__header');
     const resultsList = document.querySelector('.search__results__list');
-    const resultsFooter = document.querySelector('.search__results__footer');
-    resultsHeader.innerHTML=''
-    resultsList.innerHTML=''
-    resultsFooter.innerHTML=''
+    // const resultsFooter = document.querySelector('.search__results__footer');
+    resultsHeader.innerHTML = ''
+    resultsList.innerHTML = ''
+    // resultsFooter.innerHTML=''
 }
 
 
